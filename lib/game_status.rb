49cde7834b59bdd65aced9c7648d9bff0e23bab4
Dennis Gonzalez <gonzalezdennis6@gmail.com>
2017-11-09 22:26:27 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -50,5 +50,8 @@ def winner(board)
 result = won?(board)
 if board[result[0]] == "X" && board[result[1]] == "X" && board[result[2]] == "X"
   "X"
+elsif if board[result[0]] == "O" && board[result[1]] == "O" && board[result[2]] == "O"
+  "O"
+else nil
 end
 end
