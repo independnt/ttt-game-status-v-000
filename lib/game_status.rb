4825dcdd547e7be48b238e3460ccaed87e8bab18
Dennis Gonzalez <gonzalezdennis6@gmail.com>
2017-11-09 22:30:51 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -52,6 +52,5 @@ if board[result[0]] == "X" && board[result[1]] == "X" && board[result[2]] == "X"
   "X"
 elsif board[result[0]] == "O" && board[result[1]] == "O" && board[result[2]] == "O"
   "O"
-else nil
 end
 end
